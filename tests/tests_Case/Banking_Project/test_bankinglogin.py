c91bdd683a5cb285694ed8e6b33a48af329d0ce5
joshi <joshi.naveen03@gmail.com>
2024-10-20 20:12:00 +0530

    first commit 20/10/2024 8:00pm

---
+++ tests/tests_Case/Banking_Project/test_bankinglogin.py
@@ -0,0 +1,15 @@
+# Assertion Here
+#start webdriver
+#Call the Page Object
+#Verification
+import pytest
+import allure
+
+from tests.pageObjects.l
+def setup():
+    pass
+
+def tear_down():
+    pass
+
+
